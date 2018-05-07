fb9e5753b78e65704392f42e791fab46797ebdd8
adolph <1138027478@qq.com>
2018-05-07 16:17:24 +0800

    add 添加thinkphp安装

---
+++ src/components/adapter/ThinkPHP.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace app\components\adapter;
+
+
+class ThinkPHP extends BaseAdapter
+{
+    // 基础模板.
+    public $composer_path = "composer create-project topthink/think=5.0.* ";
+
+    public function install($baseDir,$projectName)
+    {
+        $command = $this->composer_path . $projectName;
+        // 执行原始输出.
+        return "cd $baseDir && " . $command;
+    }
+
+    public function init()
+    {
+        // TODO: Implement init() method.
+    }
+
+    public function init_mysql($baseDir,$config)
+    {
+        $db_file = $baseDir . '/config/database.php';
+        if(!is_file($db_file) || !is_writable($db_file)){
+            throw new \Exception('没有找到配置文件哦!!!');
+        }
+        $content = file_get_contents($db_file);
+        $content = preg_replace('/(127\.0\.0\.1)/',$config['host'],$content);
+        $content = preg_replace("/(database'\s+\=\>\s+\')\'","$1{$config['db_name']}'",$content);
+        $content = preg_replace("/(username\'\s+\=\>\s+')root\'","$1{$config['username']}'",$content);
+        $content = preg_replace("/(password\'\s+\=\>\s+')\'","$1{$config['password']}'",$content);
+        $content = preg_replace("/(hostport\'\s+\=\>\s+')\'","$1{$config['port']}'",$content);
+        file_put_contents($db_file,$content);
+        return true;
+    }
+}
